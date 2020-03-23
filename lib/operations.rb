c3559ad6d2424a2c937749bb01718b71dc0354f5
Justin Winger <justinwinger4@gmail.com>
2020-03-23 20:20:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed < 40 && speed < 60
+  if speed > 40 && speed > 60
     return true
   else 
     return true
@@ -8,7 +8,7 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 TRUE : false 
+	speed > 60 true : false 
 end
 	
 
